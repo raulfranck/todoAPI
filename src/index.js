344c0e93414be3f16ecf5a60e29a99220a63b640
raulfranck <raulengfranck@gmail.com>
2021-03-10 19:43:41 -0300

    Criar usuário / Verificar se usuário existe

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const usersAlradyExists = users.some((user) => user.username === username)
+
+  if (usersAlradyExists) {
+    return response.status(400).json({error: "Usuário já existe!"})
+  }
+
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todo: []
+  });
+  console.log(users)
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
